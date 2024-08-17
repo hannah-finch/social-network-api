99815f1e15841599dfce1893da67fa7e6f4b5c40
hannahschwen <hannahschwen@gmail.com>
2024-08-16 21:00:27 -0400

    wrote thought schema
    I need to add a getter method to format the timestamp

--- models/Thought.js
+++ models/Thought.js
@@ -3,9 +3,35 @@ const reactionSchema = require('./Reaction')
 
 const thoughtSchema = new Schema(
   {
-
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 120,
+      minLength: 1,
+    },
+    // TODO Use a getter method to format the timestamp on query
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    //the user that created this thought
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
   }
 );
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length
+})
 
 const Thought = model('thought', thoughtSchema);
 
