71f30b5b57fa468ac755fc0ca30756566c982fe8
hannahschwen <hannahschwen@gmail.com>
2024-08-19 14:18:26 -0400

    wrote thought controller functions
    they haven't been tested yet

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -4,7 +4,8 @@ const { Thought, User } = require('../models');
 module.exports = {
   async getThoughts(req, res) {
     try {
-
+      const thoughts = await Thought.find();
+      res.json(thoughts);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -20,7 +21,14 @@ module.exports = {
 //  (don't forget to push the created thought's _id to the associated user's thoughts array field)
   async createThought(req, res) {
     try {
+      const thought = await Thought.create(req.body);
 
+      const user = await User.findOneAndUpdate(
+        { _id: userId },
+        { $addToSet: { thoughts: thought._id }},
+        { runValidators: true, new: true }
+      )
+      res.json(thought);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -28,7 +36,16 @@ module.exports = {
 
   async getSingleThought(req, res) {
     try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId})
+        .select('-__v')
+      
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with that ID' })
+        };
 
+        res.json({
+          thought
+        });
     } catch (err) {
       res.status(500).json(err);
     }
@@ -36,7 +53,17 @@ module.exports = {
 
   async updateThoughtById(req, res) {
     try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
 
+      if (!thought) {
+        res.status(404).json({ message: 'No thought with this id' });
+      }
+
+      res.json(thought);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -44,7 +71,13 @@ module.exports = {
 
   async deleteThoughtById(req, res) {
     try {
+      const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
 
+      if (!thought) {
+        res.status(404).json( { message: 'No thought with that ID' })
+      }
+
+      res.json({ message: `Thought deleted` })
     } catch (err) {
       res.status(500).json(err);
     }
